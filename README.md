3d04f6d39e90468892289fafe5a4c3c2cbb565d7
Peter-32 <peter@impactradius.com>
2018-10-10 16:13:31 -0700

    a

--- README.md
+++ README.md
@@ -2,26 +2,37 @@
 
 ### Question
 
-
+Is Binder a good option for an interactive dashboard?
 
 ### Hypothesis
 
-
+It works well to allow the public to access to your dashboard.  It also has privacy settings
 
 ### Data Sources
 
-
+iris dataset
 
 ### Data Descriptions
 
+##### Features
 
+- sepal length (cm)
+- sepal width (cm)
+- petal length (cm)
+- petal width (cm)
+
+##### Label
+
+- Three types of flowers
 
 ### Steps
 
-
+- Data Explorer
+- Choose favorite plots
+- Create those plots in Seaborn
+- Download the dashboard plugin into commons (if not already there)
+- Git push
+- Upload to binder.com
+- Upload the interactive dashboard as an iframe to blog and set as auto exploration category
 
 ### Conclusion
-
-
-
-
---
+++ main.py
@@ -0,0 +1,24 @@
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import missingno as msno
+from sklearn.preprocessing import StandardScaler
+from pandasql import sqldf
+pd.set_option('display.mpl_style', 'default')
+pd.set_option('display.line_width', 5000)
+pd.set_option('display.max_columns', 60)
+%matplotlib inline
+q = lambda q: sqldf(q, globals())
+from sklearn.metrics import mean_squared_error
+from sklearn import linear_model, neighbors, tree, svm, ensemble
+from xgboost import XGBRegressor
+from sklearn.pipeline import make_pipeline
+from tpot.builtins import StackingEstimator
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn import datasets
+mpl.rcParams['figure.figsize'] = (15.0, 5.0)
+
+iris = datasets.load_iris()
